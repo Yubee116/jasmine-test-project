adf253a08b664cb9bb81c415b6ea385fc1c0aa43
Yubee116 <ubongnehemiah@gmail.com>
2022-02-19 11:17:08 +0100

    add functions to get, add and delete contacts from/to addressbook

--- src/AddressBook.js
+++ src/AddressBook.js
@@ -2,3 +2,16 @@ function AddressBook(){
     this.contacts = [];
     
 }
+
+AddressBook.prototype.getContact = function(index){
+    return this.contacts[index];
+
+}
+
+AddressBook.prototype.addContact = function(contact){
+    this.contacts.push(contact);
+}
+
+AddressBook.prototype.deleteContact = function(index){
+    this.contacts.splice(index,1);
+}
